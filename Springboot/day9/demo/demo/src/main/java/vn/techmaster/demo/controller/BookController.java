d7f5adfad20bd1f8bde989b4629d13457d5687d1
Huongin <huongtranvn1109@gmail.com>
2025-02-27 20:15:14 +0900

    update

---
+++ Springboot/day9/demo/demo/src/main/java/vn/techmaster/demo/controller/BookController.java
@@ -0,0 +1,37 @@
+package vn.techmaster.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import vn.techmaster.demo.model.Book;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController // Đánh dấu lên class --> Class này sẽ xử lý request và trả về response(controller)
+
+public class BookController {
+    private List<Book> books = new ArrayList<>(List.of(
+            new Book("1", "Dế mèn phiêu lưu ký", "Tô Hoài", 2000),
+            new Book( "2", "Doremon", "Fujiko F. Fujio", 1970),
+            new Book( "3", "Conan", "Gosho Aoyama",  1994)
+    ));
+
+    // 1. Lấy danh sách book: GET - /books
+    @GetMapping("/books")//HTTP method + API URL
+    public List<Book> getAllBooks(){
+        return books;
+    }
+
+    // 2. Lấy chi tiết book theo id: GET - /books/{id}
+    @GetMapping("/books/{id}")
+    public Book getBookById(@PathVariable String id){
+        for (Book book: books) {
+            if(book.getId().equals(id)) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
